11c3f60e5c74c480b5a0051bdbcb8edab1cd4dcb
TouheedNaveed <braja7059@gmail.com>
2026-08-31 18:01:52 +0500

    Configure Android build scheme to HTTP in capacitor.config.ts and remove redundant config.json

--- mobile-pwa/capacitor.config.ts
+++ mobile-pwa/capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.xdeck.mobile',
   appName: 'XDECK',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    androidScheme: 'http',
+    cleartext: true,
+    allowNavigation: ['*']
+  }
 };
 
 export default config;
